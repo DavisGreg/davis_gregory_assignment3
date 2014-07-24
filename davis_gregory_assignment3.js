1c28356a93476fa06a8f8e5a1e48d62b7a91de66
Gregory Davis <DavisGreg@fullsail.edu>
2014-07-24 17:23:07 -0500

    Included messaging features
    Included messaging features to allow user to add profiles & post from
    each profile.

--- davis_gregory_assignment3.js
+++ davis_gregory_assignment3.js
@@ -23,6 +23,12 @@ memberPW = "Enter a password number, above 100000";
 memberError = "Invalid Entry, please try again or sign up to access 'My Online Profile.'";
 memberPWordValid = false;
 
+var onlineMembers;
+var onlineMessage;
+
+onlineMembers = 10;
+onlineMessage = "There are " + onlineMembers + " users online right now.";
+
 if (welcomeEntry < 100000) {
 	memberIDValid = true;
 	while (memberPWordValid === false) {
@@ -36,41 +42,63 @@ if (welcomeEntry < 100000) {
 			console.log(memberMessage);
 		};
 	};
+	onlineMembers = onlineMembers + 1;
 } else {
 	memberIDValid = false;
 	memberMessage = memberError;
 };
 
 console.log(memberMessage);
+console.log(onlineMessage);
 	
-	
-// members online
-var onlineMembers;
-var onlineMessage;
 
 // Messaging
 
+var createProfiles;
+var messagingProfiles;
+var checkPosts;
+var postAsk;
+var postHere;
+var postAnother;
+
+createProfiles = confirm("Would you like to add profiles to your account?");
+
+if (createProfiles === true) {
+	var messaging = function(profile) {
+		var addProfiles = prompt("How many profiles would you like to add?", "Enter a whole number lower than 10.");
+		addProfiles = parseInt(addProfiles);
+		var profileNumber = 0;	
+		console.log(addProfiles + " profiles created.");
+	
+		for (var messages = [addProfiles]; [addProfiles] > profileNumber; profileNumber++) {
+			var postAsk = "Would you like to post a message in ";
+			var postHere = "Post message here.";
+			var profileName = [profileNumber];
+			profileName = parseInt(profileName);
+			var usersName = [profileNumber];
+			usersName = parseInt(usersName);
+			var createdProfile = prompt("Enter profile name.", "Profile name");
+			var profileUser = prompt("Enter User Name.", "User name");
+			var postAMessage = prompt(postAsk + createdProfile + "?", postHere);
+			console.log("Profile Name: " + createdProfile + ". User Name: " + profileUser + ". Message: " + postAMessage);
+			postAnother = confirm("Would you like to post another message in " + createdProfile + "?");
+			if (postAnother === true) {
+				var postCount = 1;
+				while (postAnother === true) {
+					var postMessageAnother = prompt("Post another message:", postHere);
+					postCount = postCount + 1;
+					console.log(createdProfile + " message " + postCount + ": " + postMessageAnother);
+					postAnother = confirm("Would you like to post another message in " + createdProfile + "?");
+				};
+			};
+		};
+		console.log(createdProfile);
+		console.log(profileUser);
+	};
+} else {
+	console.log("User " + welcomeEntry + " decided not to add profiles at this time.");
+};
 
 
-
-
-// Email
-
-
-
-
-
-// Profiles
-
-
-
-
-
-// Post profile(s)
-
-
-
-
-// Check important Dial Management messages
-
+messaging(messagingProfiles);
 
